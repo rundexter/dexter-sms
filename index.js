3c99eda039849a8af993e6a5bc881c503ddd62a5
Daniel Ilkovich <daniel+github@betaworks.com>
2015-06-29 19:57:37 -0400

    initial

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+var restler = require('restler')
+;
+
 module.exports = {
   init: function() {
   }
-  , run: function(data) {
-      return this.complete(data);
+  , run: function(step, dexter) {
+      var url     = dexter.url('home')
+        , api_key = dexter.user('api_key')
+        , to      = step.input('to')
+        , msg     = step.input('message')
+      ;
+
+      if(!url)     return this.fail('home url required');
+      if(!api_key) return this.fail('api_key required');
+      if(!to)      return this.fail('to number required');
+      if(!msg)     return this.fail('message required');
+
+      restler.post(url+'api/sms', {
+        data: {
+            to      : to
+          , message : msg
+        }
+        , headers: {
+            'X-Authorization': api_key
+        }
+      }).on('complete', function(result, response) {
+          return response.statusCode == 200 
+            ? this.complete({})
+            : this.fail({ message: 'Unexpected Response From Server' });
+      }.bind(this));
   }
 };
