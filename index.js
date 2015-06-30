d28228ca8f6f43daeac60b96fd9fc8933ea74b0c
Daniel Ilkovich <daniel+github@betaworks.com>
2015-06-30 14:29:27 -0400

    update property names, error messages

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ module.exports = {
   init: function() {
   }
   , run: function(step, dexter) {
-      var url     = dexter.url('home')
-        , api_key = dexter.user('api_key')
-        , to      = step.input('to')
+      var api_key = dexter.user('profile').api_key
+        , to      = step.input('phone_number')
         , msg     = step.input('message')
+        , url     = dexter.url('home')+'api/sms/?api_key='+api_key
       ;
 
       if(!url)     return this.fail('home url required');
       if(!api_key) return this.fail('api_key required');
-      if(!to)      return this.fail('to number required');
+      if(!to)      return this.fail('phone_number required');
       if(!msg)     return this.fail('message required');
 
-      restler.post(url+'api/sms', {
+      console.log(url);
+
+      restler.post(url, {
         data: {
             to      : to
           , message : msg
@@ -26,8 +28,8 @@ module.exports = {
         }
       }).on('complete', function(result, response) {
           return response.statusCode == 200 
-            ? this.complete({})
-            : this.fail({ message: 'Unexpected Response From Server' });
+            ? this.complete({noop: true})
+            : this.fail({ message: 'Unexpected Response From Server '+response.statusCode + result });
       }.bind(this));
   }
 };
